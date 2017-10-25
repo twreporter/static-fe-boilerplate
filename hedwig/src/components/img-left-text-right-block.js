b7407d9d6148aaa9f2c4cafee664fec2e03bd863
nickhsine <nickhsine97753017@gmail.com>
2017-10-25 21:19:32 +0800

    Side bar and ImgLeftTextRightBlock

---
+++ hedwig/src/components/img-left-text-right-block.js
@@ -0,0 +1,352 @@
+import ImgWrapper from './img-wrapper'
+import PropTypes from 'prop-types'
+import React, { PureComponent } from 'react'
+import styled from 'styled-components'
+import { screen } from './styles/utils'
+
+const mockup = {
+  hd: {
+    width: '1133px',
+    img: {
+      width: '569px',
+      height: '804px',
+    },
+    interview: {
+      width: '498px',
+    }
+  },
+  desktop: {
+    width: '868px',
+    img: {
+      width: '400px',
+      height: '566px',
+    },
+    interview: {
+      width: '431px',
+    }
+  },
+  tablet: {
+    width: '556px',
+    img: {
+      width: '556px',
+      height: '786px',
+    },
+    interview: {
+      width: '556px',
+    }
+  },
+  mobile: {
+    interview: {
+      width: ((278/320)*100)+'%'
+    }
+  }
+}
+
+const OuterContainer = styled.div`
+  width: 100%;
+  background-color: #FFFFFF;
+  margin: 0 auto;
+  padding: 60px 0;
+
+  ${screen.desktopBelow`
+    padding: 30px 0;
+  `}
+`
+
+const InnerContainer = styled.div`
+  color: #4a4949;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  ${screen.desktopBelow`
+    display: block;
+    width: 100%;
+  `}
+`
+
+const DesktopAboveHide = styled.div`
+  ${screen.desktopAbove`
+    display: none;
+  `}
+`
+
+const DesktopBelowHide = styled.div`
+  ${screen.desktopBelow`
+    display: none;
+  `}
+`
+
+const TabletAboveHide = styled.div`
+  ${screen.tabletAbove`
+    display: none;
+  `}
+`
+
+const ImgDesContainer = styled.div`
+  ${screen.desktopBelow`
+    width: ${mockup.tablet.img.width};
+    margin: 0 auto;
+    padding-bottom: 40px;
+  `}
+  ${screen.desktopAbove`
+    flex: 0 0 ${mockup.desktop.img.width};
+    margin-right: 37px;
+  `}
+  ${screen.hdAbove`
+    flex: 0 0 ${mockup.hd.img.width};
+    margin-right: 67px;
+  `}
+  ${screen.tabletBelow`
+    width: 100%;
+    padding-bottom: 45px;
+  `}
+`
+
+const ImgContainer = styled.div`
+  ${screen.tabletAbove`
+    height: ${mockup.tablet.img.height};
+  `}
+  ${screen.desktopAbove`
+    height: ${mockup.desktop.img.height};
+  `}
+  ${screen.hdAbove`
+    height: ${mockup.hd.img.height};
+  `}
+  ${screen.tabletBelow`
+    height: auto;
+  `}
+`
+
+const InterviewsContainer = styled.div`
+  ${screen.desktopBelow`
+    background-color: #e2e2e2;
+    padding-bottom: 60px;
+    padding-top: 60px;
+    width: 100%;
+  `}
+  ${screen.desktopAbove`
+    flex: 0 0 ${mockup.desktop.interview.width};
+  `}
+  ${screen.hdAbove`
+    flex: 0 0 ${mockup.hd.interview.width};
+  `}
+`
+
+const AuthorContainer = styled.div`
+  position: relative;
+  font-weight: bold;
+  margin-bottom: ${props => props.marginBottom || '70px'};
+  text-align: ${props => props.textAlign || 'left'};
+
+  ${screen.desktopBelow`
+    text-align: center;
+    margin-bottom: 30px;
+  `}
+
+  ${screen.tabletBelow`
+    text-align: left;
+    margin-bottom: 25px;
+    width: ${mockup.mobile.interview.width};
+    margin-right: auto;
+    margin-left: auto;
+  `}
+
+  ${screen.desktopAbove`
+    text-align: left;
+    margin-bottom: 70px;
+  `}
+
+`
+
+const ProgressIndicator = styled.div`
+  position: absolute;
+  top:0 ;
+  right: 0;
+`
+
+const Author = styled.div`
+  font-size: 35px;
+`
+
+const JobTitle = styled.div`
+  font-size: 16px;
+`
+
+const InterviewContainer = styled.div`
+  margin-bottom: 60px;
+
+  :last-child {
+    margin-bottom: 0;
+  }
+
+  text-align: justify;
+  ${screen.tabletBelow`
+    width: ${mockup.mobile.interview.width};
+    margin: 0 auto;
+  `}
+  ${screen.desktopBelow`
+    max-width: 556px;
+    margin: 0 auto 60px auto;
+  `}
+`
+
+const Question = styled.div`
+  font-size: 23px;
+  font-weight: bold;
+  line-height: 1.7;
+  margin-bottom: 15px;
+`
+
+const Answer = styled.p`
+  font-size: 18px;
+  line-height: 1.94;
+  margin-bottom: 60px;
+  margin: 0;
+`
+
+const IllustrationDesc = styled.div`
+  text-align: left;
+  width: 100%;
+
+  ${screen.desktopBelow`
+    text-align: right;
+  `}
+
+  ${screen.tabletBelow`
+    text-align: left;
+    padding-left: 25px;
+  `}
+`
+
+const ThinSpan = styled.span`
+  font-size: ${props => props.fontSize || '12px'};
+  font-weight: 300;
+  line-height: 2.92;
+  white-space: pre-wrap;
+`
+
+const BoldSpan = styled.span`
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 2.5;
+  white-space: pre-wrap;
+`
+
+export default class ImgLeftTextRightBlock extends PureComponent {
+  static propTypes = {
+    author: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      jobTitle: PropTypes.string,
+      country: PropTypes.string,
+      email: PropTypes.string,
+      website: PropTypes.string,
+    }).isRequired,
+    illustration: PropTypes.shape({
+      image: PropTypes.shape({
+        resizedTargets: PropTypes.shape({
+          mobile: PropTypes.string,
+          tablet: PropTypes.string,
+          desktop: PropTypes.string,
+        }),
+        alt: PropTypes.string,
+      }),
+      materials: PropTypes.string,
+      size: PropTypes.shape({
+        height: PropTypes.string,
+        width: PropTypes.string,
+      }),
+      comment: PropTypes.string,
+    }).isRequired,
+    interviews: PropTypes.arrayOf(PropTypes.shape({
+      question: PropTypes.string.isRequired,
+      answer: PropTypes.arrayOf(PropTypes.string),
+    })).isRequired,
+    ProgressSVG: PropTypes.func.isRequired,
+  }
+
+  render() {
+    const { ProgressSVG, author, illustration, interviews } = this.props
+    const interviewsJSX = Array.isArray(interviews) ? interviews.map((interview, index) => {
+      const answers = interview.answer
+      const answersJSX = Array.isArray(answers) ? answers.map((answer, index) => {
+        return (
+          <Answer key={`answer_${index}`}>{answer}</Answer>
+        )
+      }) : null
+
+      return (
+        <InterviewContainer key={`interview_${index}`}>
+          <Question>{interview.question}</Question>
+          <div>
+            {answersJSX}
+          </div>
+        </InterviewContainer>
+      )
+    }) : null
+    return (
+      <OuterContainer>
+        <InnerContainer>
+          <ImgDesContainer>
+            <DesktopAboveHide>
+              <AuthorContainer
+                textAlign="left"
+                marginBottom="30px"
+              >
+                <Author>
+                  {author.name}
+                </Author>
+                <JobTitle>
+                  {author.jobTitle}，{author.country}
+                </JobTitle>
+                <TabletAboveHide>
+                  <ProgressIndicator>
+                    <ProgressSVG />
+                  </ProgressIndicator>
+                </TabletAboveHide>
+              </AuthorContainer>
+            </DesktopAboveHide>
+            <ImgContainer>
+              <ImgWrapper
+                alt={illustration.alt}
+                src={illustration.image.resizedTargets.tablet}
+              >
+              </ImgWrapper>
+            </ImgContainer>
+            <IllustrationDesc>
+              <div>
+                <ThinSpan>媒材  </ThinSpan>
+                <BoldSpan>{illustration.materials}</BoldSpan>
+                <ThinSpan fontSize="14px">  |  </ThinSpan>
+                <ThinSpan>作品尺寸  </ThinSpan>
+                <BoldSpan>{illustration.size.width} X {illustration.size.height}</BoldSpan>
+              </div>
+              <div>
+                <ThinSpan>{author.email}</ThinSpan>
+              </div>
+              <div>
+                <ThinSpan>{illustration.comment}</ThinSpan>
+              </div>
+            </IllustrationDesc>
+          </ImgDesContainer>
+          <InterviewsContainer>
+            <DesktopBelowHide>
+              <AuthorContainer
+                textAlign="center"
+              >
+                <Author>
+                  {author.name}
+                </Author>
+                <JobTitle>
+                  {author.jobTitle}，{author.country}
+                </JobTitle>
+              </AuthorContainer>
+            </DesktopBelowHide>
+            {interviewsJSX}
+          </InterviewsContainer>
+        </InnerContainer>
+      </OuterContainer>
+    )
+  }
+}
+
---
+++ hedwig/src/components/side-bar.js
@@ -0,0 +1,212 @@
+import get from 'lodash/get'
+import PropTypes from 'prop-types'
+import React from 'react'
+import Waypoint from 'react-waypoint'
+import smoothScroll from 'smoothscroll'
+import styled from 'styled-components'
+import { screen } from './styles/utils'
+
+const _ = {
+  get,
+}
+
+const Container = styled.div`
+  background-color: #FFFFFF;
+  height: 100%;
+  left: 0px;
+  position: fixed;
+  top: 0px;
+  z-index: 100;
+  ${screen.mobileOnly`
+    display: none;
+  `}
+`
+
+const AnchorsContainer = styled.div`
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+`
+
+const Anchor = styled.div`
+  padding: 20px;
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+const Label = styled.div`
+  color: #a67a44;
+  font-size: 13px;
+  font-weight: bold;
+  line-height: 1.5;
+  width: 13px;
+  border-bottom: ${props => (props.highlight ? 'solid 4px #a67a44' : '')};
+`
+
+class Anchors extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentSection: _.get(this.props, 'data.0.id'),
+    }
+    this.changeHighlight = this._changeHighlight.bind(this)
+  }
+
+  _changeHighlight(currentSection) {
+    this.setState({
+      currentSection,
+    })
+  }
+
+  render() {
+    const AssembleWord = (words) => {
+      return words.split('').map((word) => {
+        return (
+          <Label key={`anchor_label_${word}`}>
+            {word}
+          </Label>
+        )
+      })
+    }
+    const anchorBts = []
+    this.props.data.forEach((anchorObj) => {
+      const moduleID = _.get(anchorObj, 'id', '')
+      const moduleLabel = _.get(anchorObj, 'label', '')
+
+      // moduleID and moduleLable are not empty string
+      if (moduleID && moduleLabel) {
+        const anchorJSX = (
+          <Anchor
+            onClick={(e) => { this.props.handleClickAnchor(moduleID, e) }}
+            key={`SectionButton_${moduleID}`}
+          >
+            <Label
+              key={`anchor_label_${moduleLabel}`}
+              highlight={moduleID === this.state.currentSection}
+            >
+              {moduleLabel}
+            </Label>
+          </Anchor>
+        )
+        anchorBts.push(anchorJSX)
+      }
+    })
+    return (
+      <AnchorsContainer>
+        { anchorBts }
+      </AnchorsContainer>
+    )
+  }
+}
+
+Anchors.defaultProps = {
+  data: [],
+}
+
+Anchors.propTypes = {
+  handleClickAnchor: PropTypes.func.isRequired,
+  data: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string,
+    label: PropTypes.string,
+  })),
+}
+
+class SideBar extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.scrollTo = this._scrollTo.bind(this)
+    this.handleOnEnter = this._handleOnEnter.bind(this)
+    this.handleOnLeave = this._handleOnLeave.bind(this)
+    // moduleID to Module
+    this.moduleMap = {}
+    this.currentSection = ''
+    this.previousSection = ''
+  }
+
+  componentWillUnmount() {
+    this.moduleMap = {}
+  }
+
+  _scrollTo(moduleID, e) {
+    e.preventDefault()
+    const node = this.moduleMap[moduleID]
+    if (node) {
+      return smoothScroll(node.offsetTop)
+    }
+    return null
+  }
+
+  _handleOnEnter(nextSection) {
+    this.anchorsNode.changeHighlight(nextSection)
+    this.previousSection = this.currentSection
+    this.currentSection = nextSection
+  }
+
+  _handleOnLeave(onLeaveSection) {
+    if (onLeaveSection === this.currentSection) {
+      this.currentSection = this.previousSection
+      this.anchorsNode.changeHighlight(this.previousSection)
+      this.previousSection = onLeaveSection
+    }
+  }
+
+  render() {
+    const { children, anchors } = this.props
+    let modules = children
+    if (children && !Array.isArray(children)) {
+      modules = [children]
+    }
+    if (this.currentSection === '') {
+      this.currentSection = _.get(anchors, '0.id')
+    }
+    const webSiteContent = modules.map((module, index) => {
+      const moduleID = _.get(anchors, [index, 'id'], `side_bar_module_${index}`)
+      return (
+        <Waypoint
+          key={moduleID}
+          onLeave={() => { this.handleOnLeave(moduleID) }}
+          onEnter={() => { this.handleOnEnter(moduleID) }}
+          fireOnRapidScroll
+          topOffset="4%"
+          bottomOffset={(index + 1) === modules.length ? '50%' : '95%'}
+        >
+          <div
+            id={moduleID}
+            ref={(node) => { this.moduleMap[moduleID] = node }}
+          >
+            {module}
+          </div>
+        </Waypoint>
+      )
+    })
+
+    return (
+      <div>
+        <Container>
+          <Anchors
+            ref={(node) => { this.anchorsNode = node }}
+            data={anchors}
+            handleClickAnchor={this.scrollTo}
+          />
+        </Container>
+        {webSiteContent}
+      </div>
+    )
+  }
+}
+
+SideBar.defaultProps = {
+  children: [],
+  anchors: [],
+}
+
+SideBar.propTypes = {
+  children: PropTypes.array,
+  anchors: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string,
+    label: PropTypes.string,
+  })),
+}
+
+export default SideBar
