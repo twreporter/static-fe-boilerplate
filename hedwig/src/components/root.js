117848820e70a251755f23b6467e996e3c243590
yucj <nakts0123@gmail.com>
2017-10-25 21:33:55 +0800

    Update root

--- hedwig/src/components/root.js
+++ hedwig/src/components/root.js
@@ -1,19 +1,53 @@
+/* eslint no-unused-expressions: 0 */
 import React from 'react'
-import styled from 'styled-components'
-import testImage from '../../static/logo.png'
+import styled, { injectGlobal } from 'styled-components'
+import Header from './header'
+import article from '../data/article'
+import Credits from './credits'
+import Article from './article'
+import section01 from '../data/section-01'
+import section02 from '../data/section-02'
+import section03 from '../data/section-03'
+import SingleColumnPictureDesktop from './single-column-picture-desktop'
+import SingleColumnPictureMobile from './single-column-picture-mobile'
+import Interview from './interview'
+
+injectGlobal`
+  body, html {
+    padding: 0;
+    margin: 0;
+  }
+`
+
+const {
+  image,
+  title,
+  content,
+  credits,
+} = article
 
 const Container = styled.div`
-  background-color: grey;
-    width: 100%;
-      text-align: center;
-      `
+  background-color: #f2f2f2;
+  width: 100%;
+  text-align: center;
+`
 
 export default class Root extends React.Component {
   render() {
     return (
       <Container>
-        <img src={testImage} alt="presentation" />
-        this is a root component
+        <Header image={image} title={title} />
+        <Credits credits={credits} />
+        <Article content={content} />
+        <SingleColumnPictureDesktop {...section01} />
+        <SingleColumnPictureMobile {...section01} />
+        <Interview {...section01} />
+        <SingleColumnPictureDesktop {...section02} />
+        <SingleColumnPictureMobile {...section02} />
+        <Interview {...section02} />
+        <SingleColumnPictureDesktop {...section03} />
+        <SingleColumnPictureMobile {...section03} />
+        <Interview {...section03} />
       </Container>
     )
   }
