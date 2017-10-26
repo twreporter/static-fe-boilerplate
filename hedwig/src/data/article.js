778d301bd618f07957931de4a741966e077a7543
yucj <nakts0123@gmail.com>
2017-10-26 17:32:35 +0800

    Fix typo

--- hedwig/src/data/article.js
+++ hedwig/src/data/article.js
@@ -1,5 +1,5 @@
 const article = {
-  title: '【搖滾芭比專題】\n我是新柏林圍牆，來拆毀我吧！\n──創作者畫筆下的Hedwig',
+  title: '【搖滾芭比專題】\n我是新柏林圍牆，來拆毀我吧！\n──創作者畫筆下的 Hedwig',
   image: {
     resizedTargets: {
       mobile: 'https://storage.googleapis.com/twreporter-infographics/hedwig-and-the-angry-inch-artist/static/leading-img-mobile.jpg',
@@ -57,10 +57,10 @@ const article = {
     },
   ],
   content: [
-    'Hasel 出生在柏林圍牆築起的那年，直到 1989 年圍牆倒塌了，人們從東德跨越到西德，Hasel 也跨越了性別的牆：他將名字改成 Hedwig ，從生理男性成了失敗手術台上僅存胯下「憤怒一吋」的變性者。戴上金色假髮擦上紅唇，用他是男也是女的身體，搖搖晃晃站在分裂性別的那堵牆，唱跳憤怒。',
-    '以東德變性人 Hedwig 為故事主角的音樂電影《搖滾芭比》（Hedwig and the angry inch），由極具個人魅力的 John Cameron Mitchell（以下簡稱 JCM）在2001年自編自導自演搬上大螢幕。題材雖從性別議題出發，故事卻超越了性別，以「人是否能尋回失落的另一半」為核心，透過一首又一首力道俱足的搖滾樂，唱出 Hedwig 分裂的一生。',
+    'Hansel 出生在柏林圍牆築起的那年，直到 1989 年圍牆倒塌了，人們從東德跨越到西德，Hansel 也跨越了性別的牆：他將名字改成 Hedwig ，從生理男性成了失敗手術台上僅存胯下「憤怒一吋」的變性者。戴上金色假髮擦上紅唇，用他是男也是女的身體，搖搖晃晃站在分裂性別的那堵牆，唱跳憤怒。',
+    '以東德變性人 Hedwig 為故事主角的音樂電影《搖滾芭比》（Hedwig and the angry inch），由極具個人魅力的 John Cameron Mitchell（以下簡稱 JCM）在 2001 年自編自導自演搬上大螢幕。題材雖從性別議題出發，故事卻超越了性別，以「人是否能尋回失落的另一半」為核心，透過一首又一首力道俱足的搖滾樂，唱出 Hedwig 分裂的一生。',
     '角色原型一半來自 JCM 兒時的家庭保姆，一半來他的自身故事。在改編成為電影之前，早在 1994 年 JCM 便與創作搭擋 Stephen Trask，在紐約著名同志夜店 Squeezebox 以小型樂隊形式讓《Hedwig and the angry inch》首次登場。包辦詞曲創作的 Stephen Trask 曾在一場採訪中談及當年紐約同志圈，還沒有太多空間留給搖滾樂，但他們創造的這部龐克搖滾音樂劇卻改變了一切。',
-    '1998 年《Hedwig and the angry inch》從小夜店舞台走進了外百老匯（Off-Broadway）；2014年美國演員 Neil Patrick Harris 主演 Hedwig 一角，正式登上紐約百老匯 Belasco 劇院，同年並獲美國劇場界最高榮譽東尼獎（Tony Award）4 項肯定。',
+    '1998 年《Hedwig and the angry inch》從小夜店舞台走進了外百老匯（Off-Broadway）；2014 年美國演員 Neil Patrick Harris 主演 Hedwig 一角，正式登上紐約百老匯 Belasco 劇院，同年並獲美國劇場界最高榮譽東尼獎（Tony Award）4 項肯定。',
     '時至今日，《Hedwig and the angry inch》音樂劇除了美國，在英國、澳洲、德國、義大利等歐洲國家，甚至亞洲的韓國與台灣，都曾改編為當地版本搬上舞台。JCM 本人也在 10 月剛結束於日本東京、大阪的特別演出，共 5 場超過萬人的席次，座無虛席。即使年過 50 歲，JCM 舞台魅力依舊不減，當他撐開華麗且拼貼的斗篷，挑釁唱著〈Tear me down〉，那個從東德失敗手術台帶著傷痕遠離的 Hedwig，彷彿穿越時空出現眼前。',
     '在台灣，電影版《搖滾芭比》並未正式於院線放映，唯二的戲院放映是 2002 年金馬影展與 2015 年的金馬奇幻影展，更多人是在某個深夜的 HBO 台一睹 Hedwig 身影，今年酷兒影展終於讓《搖滾芭比》重返大螢幕。',
     '《報導者》藉著戲院放映之際企劃了「搖滾芭比專題」，有別過往以文字談電影，我們邀請了 6 位喜愛電影且擅長繪畫的創作者，用他們具才華的筆，畫下最觸動他們的 Hedwig。僅以此專題，向所有這個時代遭受各種分裂與惡意，卻依然保有柔軟且強大意志的每一個人、每一顆心致意。',
