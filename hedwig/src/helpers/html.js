4fe3d93f0e2c0c1142050bf22290eb9bfb03ec61
yucj <nakts0123@gmail.com>
2017-10-26 17:32:56 +0800

    Add GA and meta infos

--- hedwig/src/helpers/html.js
+++ hedwig/src/helpers/html.js
@@ -3,6 +3,11 @@
 import PropTypes from 'prop-types'
 import React, { PureComponent } from 'react'
 
+const title = '【搖滾芭比專題】我是新柏林圍牆，來拆毀我吧！──創作者畫筆下的Hedwig'
+const description = '人是否能尋回失落的另一半？我們邀請了6位創作者，用他們的筆，畫下最觸動他們的Hedwig。'
+const ogImage = 'https://storage.googleapis.com/twreporter-infographics/hedwig-and-the-angry-inch-artist/static/leading-img-desktop.jpg'
+const ogUrl = 'https://www.twreporter.org/i/hedwig-and-the-angry-inch-artist'
+
 export default class Html extends PureComponent {
   static propTypes = {
     scripts: PropTypes.arrayOf(PropTypes.string).isRequired,
@@ -15,16 +20,37 @@ export default class Html extends PureComponent {
     return (
       <html lang="zh-TW">
         <head>
+          <title>{title}</title>
+          <meta name="description" content={description} />
+          <meta name="twitter:title" content={title} />
+          <meta name="twitter:image" content={ogImage} />
+          <meta name="twitter:description" content={description} />
+          <meta property="og:title" content={title} />
+          <meta property="og:description" content={description} />
+          <meta property="og:image" content={ogImage} />
+          <meta property="og:type" content="article" />
+          <meta property="og:url" content={ogUrl} />
+          <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1, initial-scale=1" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="theme-color" content="#E30B20" />
           <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1, initial-scale=1" />
           <meta charSet="utf-8" />
           <style dangerouslySetInnerHTML={{ __html: styleTags }} />
           <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css" media="all" rel="stylesheet" type="text/css" charSet="UTF-8" />
+          <link href="https://www.twreporter.org/images/apple-touch-icon.png" rel="apple-touch-icon" />
+          <link href="https://www.twreporter.org/images/apple-touch-icon-152x152.png" rel="apple-touch-icon" sizes="152x152" />
+          <link href="https://www.twreporter.org/images/apple-touch-icon-167x167.png" rel="apple-touch-icon" sizes="167x167" />
+          <link href="https://www.twreporter.org/images/apple-touch-icon-180x180.png" rel="apple-touch-icon" sizes="180x180" />
+          <link href="https://www.twreporter.org/images/icon-hires.png" rel="icon" sizes="192x192" />
+          <link href="https://www.twreporter.org/images/icon-normal.png" rel="icon" sizes="128x128" />
         </head>
         <body>
           <div id="root" dangerouslySetInnerHTML={{ __html: content }} />
           {scripts.map(script => <script key={script} type="text/javascript" src={script} />)}
           <script src="https://use.typekit.net/sna3sjs.js" />
           <script dangerouslySetInnerHTML={{ __html: 'try{Typekit.load({ async: true });}catch(e){}' }} />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-69336956-1" />
+          <script dangerouslySetInnerHTML={{ __html: 'window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag(\'js\', new Date()); gtag(\'config\', \'UA-69336956-1\');' }} />
         </body>
       </html>
     )
