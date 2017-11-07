f21f10b7416479044abbe166acef43664c57d59b
yucj <nakts0123@gmail.com>
2017-11-07 11:47:36 +0800

    [child-in-relic] Update html

--- child-in-relic/src/helpers/html.js
+++ child-in-relic/src/helpers/html.js
@@ -2,6 +2,14 @@
 
 import PropTypes from 'prop-types'
 import React, { PureComponent } from 'react'
+import pageMeta from '../data/page-meta'
+
+const {
+  title,
+  description,
+  ogImage,
+  ogUrl,
+} = pageMeta
 
 export default class Html extends PureComponent {
   static propTypes = {
@@ -15,15 +23,34 @@ export default class Html extends PureComponent {
     return (
       <html lang="zh-TW">
         <head>
+          <title>{title}</title>
+          <meta name="description" content={description} />
+          <meta name="twitter:title" content={title} />
+          <meta name="twitter:image" content={ogImage} />
+          <meta name="twitter:description" content={description} />
+          <meta property="og:title" content={title} />
+          <meta property="og:description" content={description} />
+          <meta property="og:image" content={ogImage} />
+          <meta property="og:type" content="article" />
+          <meta property="og:url" content={ogUrl} />
           <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1, initial-scale=1" />
           <meta charSet="utf-8" />
           <style dangerouslySetInnerHTML={{ __html: styleTags }} />
+          <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css" media="all" rel="stylesheet" type="text/css" charSet="UTF-8" />
+          <link href="https://www.twreporter.org/images/apple-touch-icon.png" rel="apple-touch-icon" />
+          <link href="https://www.twreporter.org/images/apple-touch-icon-152x152.png" rel="apple-touch-icon" sizes="152x152" />
+          <link href="https://www.twreporter.org/images/apple-touch-icon-167x167.png" rel="apple-touch-icon" sizes="167x167" />
+          <link href="https://www.twreporter.org/images/apple-touch-icon-180x180.png" rel="apple-touch-icon" sizes="180x180" />
+          <link href="https://www.twreporter.org/images/icon-hires.png" rel="icon" sizes="192x192" />
+          <link href="https://www.twreporter.org/images/icon-normal.png" rel="icon" sizes="128x128" />
         </head>
         <body>
           <div id="root" dangerouslySetInnerHTML={{ __html: content }} />
           {scripts.map(script => <script key={script} type="text/javascript" src={script} />)}
           <script src="https://use.typekit.net/sna3sjs.js" />
           <script dangerouslySetInnerHTML={{ __html: 'try{Typekit.load({ async: true });}catch(e){}' }} />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-69336956-1" />
+          <script dangerouslySetInnerHTML={{ __html: 'window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag(\'js\', new Date()); gtag(\'config\', \'UA-69336956-1\');' }} />
         </body>
       </html>
     )
