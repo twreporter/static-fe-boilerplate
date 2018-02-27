315f1704779f6048478e1039ad641d6bc7f2d784
nickhsine <nickhsine97753017@gmail.com>
2018-02-27 17:54:44 +0800

    enable hotjar tracking

--- southeast-asia-literature/src/helpers/html.js
+++ southeast-asia-literature/src/helpers/html.js
@@ -71,6 +71,18 @@ export default class Html extends PureComponent {
           <div id="root" dangerouslySetInnerHTML={{ __html: content }} />
           {scripts.map(script => <script key={script} type="text/javascript" src={script} />)}
           <script dangerouslySetInnerHTML={{ __html: typeKitCode }} />
+          <script dangerouslySetInnerHTML={{
+            __html: `(function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:376929,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+          />
         </body>
       </html>
     )
